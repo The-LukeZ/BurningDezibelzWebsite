e8a9660533f6109eba239e1d240a97311bd07412
LukeZNotFound <luke.hent3005@gmail.com>
2024-10-28 18:50:57 +0100

    Add @sveltejs/adapter-cloudflare dependency and update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-node";
+// import nodeAdapter from "@sveltejs/adapter-node";
+import CFAdapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +9,12 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-            assets: "static",
-            precompress: false,
+        adapter: CFAdapter({
+            routes: {
+                include: ["/src/routes/**/*"],
+                exclude: ["<all>"],
+            },
+            fallback: "plaintext",
         }),
     },
     preprocess: vitePreprocess,
